52f19254a5156f44f6cf56465ddcb25ea0f7f21b
tuanguyen <tuanguyen@axon.com>
2023-04-11 08:44:49 +0700

    cleanup

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,11 +4,6 @@ class Document extends NextDocument {
   render = () => (
     <Html lang="ja">
       <Head>
-        <title>Knowledge Society</title>
-        <meta
-          name="description"
-          content="Knowledge Society is a large and spacious coworking space, virtual office, and rental office company which is located less than one minute walk from Kudanshita station in down town Tokyo."
-        />
         <link
           rel="icon"
           href="/favicon.ico"
